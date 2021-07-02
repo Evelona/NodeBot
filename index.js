40d5f669a87d4c0c5007ed72f38665b824fb8cf3
Eveline <exampla@gmail.com>
2021-07-02 23:59:02 +0300

    repeat bot

---
+++ index.js
@@ -0,0 +1,11 @@
+const telegramApi = require('node-telegram-bot-api')
+
+const token = '1788090621:AAFe2OpEt8ZH97tvUBIflzA56k1tJDF_HyI'
+
+const bot = new telegramApi(token, {polling: true})
+
+bot.on('message', msg => {
+	const text = msg.text;
+	const chatId =  msg.chat.id
+	bot.sendMessage(chatId, `Привет, я не расслышал... Ты ${text} сказал`)
+})
